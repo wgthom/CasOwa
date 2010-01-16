21592515c52e823b33834c38f4b2798702be8a88
William G. Thompson, Jr <wthompso@princeton.edu>
2010-01-15 22:04:37 -0500

    .gitignore settings and AssemblyInfo update

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CasOwa")]
-[assembly: AssemblyDescription("Jasig CAS Client for OWA")]
+[assembly: AssemblyDescription("DotNet CAS Client for OWA")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CasOwa")]
